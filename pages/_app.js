0f49db69f60fee63f0fa16a9f62f79bdeba3d791
VargentDi <zdxuan123@gmail.com>
2020-08-23 22:28:18 -0500

    style initial

--- pages/_app.js
+++ pages/_app.js
@@ -7,5 +7,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
+MyApp.getInitialProps = async ({Component,ctx} ) => {
+  // const res = await fetch('')
+  // const data = await res.json()
+  let pageProps={}
+  if(Component.getInitialProps){
+    pageProps = await Component.getInitialProps(ctx)
+  }
+  return { pageProps }
+}
 export default MyApp
